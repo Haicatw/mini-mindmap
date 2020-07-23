56ac7b9cba54c2641be5c8081e14665bf0e2e81e
kirbyzhou <kirbyzhou@protonmail.com>
2020-07-23 19:23:33 +0000

    Switch to yarn and add sidebar

---
+++ src/components/MindMap.vue
@@ -0,0 +1,14 @@
+<template>
+  <mindmap
+    :nodes="nodes"
+    :connections="connections"
+    :editable="true"
+  />
+</template>
+
+<script>
+  export default {
+    name: 'MindMap',
+    props: ['nodes', 'connections', 'editable']
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+import Vuesax from 'vuesax'
+import 'vuesax/dist/vuesax.css'
 
-Vue.config.productionTip = false;
+Vue.config.productionTip = false
 
-new Vue({
+new Vue(Vuesax, {
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app')
