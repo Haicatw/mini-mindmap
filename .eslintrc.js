9d23987d27e7413c8b3a93d31dfadeb1790a90af
kirbyzhou <kirbyzhou@protonmail.com>
2020-08-05 17:06:20 +0000

    Refresh repo for new arch

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,14 @@
 module.exports = {
   root: true,
   env: {
-    browser: true,
     node: true
   },
-  parserOptions: {
-    parser: 'babel-eslint'
-  },
   extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
-  plugins: ['prettier'],
-  // add your custom rules here
-  rules: {}
-}
+  parserOptions: {
+    parser: "babel-eslint"
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  }
+};
